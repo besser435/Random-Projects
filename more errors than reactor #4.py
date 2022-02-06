a2e9be5cf04075b80925b0685ae1e92eee956fb5
Brandon <68145204+besser435@users.noreply.github.com>
2022-02-06 01:22:45 -0700

    Create more errors than reactor #4.py

---
+++ more errors than reactor #4.py
@@ -0,0 +1,12 @@
+from colorama import Fore, Back, Style
+import traceback
+
+
+print(Fore.RED + "Reactor #4 time")
+
+while True:
+    try:
+        gigachad_300GW_time
+    except Exception:
+        print(Fore.YELLOW + traceback.format_exc())
+        print(Fore.RED + traceback.format_exc())

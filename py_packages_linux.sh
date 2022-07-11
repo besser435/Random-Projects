b4fd45ce4d4a3402ed715fba2b796b911d0313ef
Brandon <brandonesser435@gmail.com>
2022-07-11 02:14:26 -0700

    update

---
+++ py_packages_linux.sh
@@ -0,0 +1,22 @@
+pip3 install colorama
+pip3 install numpy
+pip3 install pygame_menu
+pip3 install keyboard
+pip3 install pillow
+pip3 install pynput
+pip3 install pygame
+pip3 install art
+pip3 install tk
+
+pip3 install gpiozero
+pip3 install picamera
+
+pip3 install Adafruit-Blinka
+pip3 install adafruit-circuitpython-displayio-ssd1306
+pip3 install adafruit-blinka-displayio
+pip3 install adafruit-circuitpython-neopixel
+pip3 install adafruit-circuitpython-icm20x
+pip3 install adafruit-circuitpython-bmp3xx
+pip3 install adafruit-circuitpython-vl53l0x
+pip3 install adafruit-circuitpython-rfm69
+read -p "Press enter to continue..."

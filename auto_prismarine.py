9d7957cd4c7838e2a5dd811db54428af3a4163d7
Brandon <brandonesser435@gmail.com>
2023-06-03 21:55:27 -0700

    Update auto_prismarine.py

--- auto_prismarine.py
+++ auto_prismarine.py
@@ -2,10 +2,9 @@ import time, pydirectinput, mouse, keyboard #,pyautogui
 from time import sleep
 # https://learncodebygaming.com/blog/pyautogui-not-working-use-directinput
 
-# time in seconds before pick refill. 4000 seems to be right for Unbreaking III
-REFILL_DELAY = 4450 #4450
+# time in seconds before pick refill. 4400 seems to be right for Unbreaking III
+REFILL_DELAY = 4400
 
-refill_counter = 0
 
 def refill_picks():
     # move to the dohickey
@@ -46,39 +45,54 @@ def refill_picks():
 
     sleep(0.5)
 
-try:
-    print("Starting in 3 seconds...")
-    sleep(3)
-    print("Mining...")
 
-    initial_time = time.monotonic()
+def read_chat():
+    pass
 
-    while True: 
-        current_time = time.monotonic()
-        time_stamp = int(current_time - initial_time + 1)    # +1 to not trigger refill_picks() on the first loop
-       
-        pydirectinput.mouseDown()
-        mouse.wheel(-1)             # cycle through picks in hotbar
-        pydirectinput.press("a")    # timeout prevention
-        sleep(1)
+def main():
+    refill_counter = 0
+    try:
+        print("Starting in 3 seconds...")
+        sleep(3)
+        print("Mining...")
 
-        # if 4000, refills picks every 1.11 hours (about the time a pick lasts)
-        if (time_stamp) % REFILL_DELAY == 0: 
-            pydirectinput.mouseUp()
-            refill_picks()
-            refill_counter += 1
-            print("Refilled")
+        initial_time = time.monotonic()
+        last_refill_time = initial_time
+        while True: 
+            current_time = time.monotonic()
+            time_stamp = current_time - initial_time + 1    # +1 to not trigger refill_picks() on the first loop
+            time_since_refill = current_time - last_refill_time
+
+            pydirectinput.mouseDown()
+            mouse.wheel(-1)             # cycle through picks in hotbar
+            pydirectinput.press("a")    # timeout prevention. Might cause times to be off by a second occasionally
+            
+            print(
+            "Refill in", int(REFILL_DELAY - time_since_refill), "seconds    ",
+            "Time elsapsed:", round((time_stamp / 3600), 2), "hours    ",
+            "Refilled", refill_counter, "times    "
+            )
+
+            sleep(1)
+
+            # if 4000, refills picks every 1.11 hours (about the time a pick lasts)
+            if time_since_refill >= REFILL_DELAY:
+                pydirectinput.mouseUp()
+                refill_picks()
+                refill_counter += 1
+                last_refill_time = current_time  
+                print("Refilled")
+            
+            read_chat()
         
-        print("Refill in", REFILL_DELAY - (time_stamp % REFILL_DELAY), "seconds")
+            if keyboard.is_pressed("f7"):
+                raise KeyboardInterrupt
+            pydirectinput.failSafeCheck()  # checks if mouse is in top left corner. If so, raises pydirectinput.FailSafeException and stops everything
 
-        if keyboard.is_pressed("f7"):
-            raise KeyboardInterrupt
-        pydirectinput.failSafeCheck()  # checks if mouse is in top left corner. If so, raises pydirectinput.FailSafeException and stops everything
-
-except KeyboardInterrupt:
-    end_time = time.time()
-    pydirectinput.mouseUp()
-    print("Done Mining")
-    print("Time elapsed:", round((time_stamp / 3600), 2), "hours")
-    print("Refilled picks", refill_counter, "times")
-    input("\nPress enter to exit...")
+    except KeyboardInterrupt:
+        pydirectinput.mouseUp()
+        print("Done Mining")
+        print("Time elapsed:", round((time_stamp / 3600), 2), "hours")
+        print("Refilled picks", refill_counter, "times")
+        input("\nPress enter to exit...")
+main()
